fb8646cd42f4111a2389112a49e75e3a2ea7b4e3
schernya <stanislav.chernyak@gmail.com>
2016-07-06 16:38:42 +0300

    added helloworld_site

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,7 @@
 import os
 import sys
+import django
+
 if __name__ == '__main__':
     print('{}'.format(os.path.split(sys.argv[0])[1]))
+    print('Django version is {}'.format(django.get_version()))
